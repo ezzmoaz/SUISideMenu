da99a0f27d6246cda0d203f02acd99fd04dccd60
Moaz Ezz <moazezz45@gmail.com>
2025-10-16 01:56:43 +0300

    CleanUps and Improve the Package structure

--- Example/Pods/Target Support Files/SUISideMenu/SUISideMenu-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SUISideMenu : NSObject
-@end
-@implementation PodsDummy_SUISideMenu
-@end
--- Package.swift
+++ Package.swift
@@ -21,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SUISideMenu",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "SUISideMenuTests",
-            dependencies: ["SUISideMenu"]),
+            dependencies: ["SUISideMenu"],
+            path: "Tests"),
     ]
 )

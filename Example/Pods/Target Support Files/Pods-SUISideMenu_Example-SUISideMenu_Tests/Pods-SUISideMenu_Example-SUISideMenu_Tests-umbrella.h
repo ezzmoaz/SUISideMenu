d8dd95b5326b7276176ac427cc97caff3f33e859
Moaaz Belal <moaaz.belal@skyresponse.com>
2025-10-16 01:56:43 +0300

    CleanUps and Improve the Package structure

--- Example/Pods/Target Support Files/Pods-SUISideMenu_Example-SUISideMenu_Tests/Pods-SUISideMenu_Example-SUISideMenu_Tests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-SUISideMenu_Example-SUISideMenu_Tests/Pods-SUISideMenu_Example-SUISideMenu_Tests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_SUISideMenu_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SUISideMenu_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_SUISideMenu_Example_SUISideMenu_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SUISideMenu_Example_SUISideMenu_TestsVersionString[];
 
--- SUISideMenu/SUISideMenu Test/Pods/Target Support Files/Pods-SUISideMenu Test/Pods-SUISideMenu Test-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SUISideMenu_Test : NSObject
-@end
-@implementation PodsDummy_Pods_SUISideMenu_Test
-@end
